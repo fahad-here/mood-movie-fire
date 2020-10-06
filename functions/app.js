9c79c43f00ecc680f3a6ab542aabdd6d3ed4a6cc
fahad_laptop <fmohajir@gmail.com>
2020-10-06 17:31:24 +0400

    added check for routes not available

--- functions/app.js
+++ functions/app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const cors = require('cors')
+const createError = require('http-errors')
 
 const indexRouter = require('./src/routes')
 const { RouteErrorHandler } = require('./src/utils')
 
 const app = express()
-
+app.use((req, res, next) => {
+    next(createError(404))
+})
 app.use(cors({ origin: true }))
 app.use('/api', indexRouter)
 // error handler
